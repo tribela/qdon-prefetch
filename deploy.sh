66d040bc825f6f7a274d7b7d4e8571ab2f64a0e9
Jeong Arm <arm@gurum.cc>
2020-01-23 08:54:08 +0900

    Update deploy.sh to deploy using latest tag

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-git_version=$(git rev-parse --short HEAD)
+# git_version=$(git rev-parse --short HEAD)
 IMAGE="kjwon15/qdon-prefetch"
-IMAGE_TAG="${IMAGE}:${git_version}"
+IMAGE_TAG="${IMAGE}:latest"
 
-if ! docker inspect --type image "${IMAGE_TAG}" &>/dev/null; then
-    docker build -t "${IMAGE_TAG}" .
-fi
+# if ! docker inspect --type image "${IMAGE_TAG}" &>/dev/null; then
+# fi
+docker build -t "${IMAGE_TAG}" .
 docker push "${IMAGE_TAG}"
 
-sed -e "s#image: ${IMAGE}.*\$#image: ${IMAGE_TAG}#" docker-compose.yml > docker-stack.yml
-docker stack deploy qdon-pfetch -c docker-stack.yml
+# sed -e "s#image: ${IMAGE}.*\$#image: ${IMAGE_TAG}#" docker-compose.yml > docker-stack.yml
+# docker stack deploy qdon-pfetch -c docker-stack.yml
+
+docker service update --image "${IMAGE_TAG}" qdon-pfetch_worker

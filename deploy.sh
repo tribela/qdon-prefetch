9d2a79238c7d6355f34881d4baa9a770df3cf0fb
Jeong Arm <arm@gurum.cc>
2020-01-22 16:49:02 +0900

    Check if exists to prevent rebuild

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 git_version=$(git rev-parse --short HEAD)
+IMAGE="kjwon15/qdon-prefetch"
+IMAGE_TAG="${IMAGE}:${git_version}"
 
-docker build -t "kjwon15/qdon-prefetch:${git_version}" .
-docker push "kjwon15/qdon-prefetch:${git_version}"
+if ! docker inspect --type image "${IMAGE_TAG}" &>/dev/null; then
+    docker build -t "${IMAGE_TAG}" .
+fi
+docker push "${IMAGE_TAG}"
 
-sed -e "s#image: kjwon15/qdon-prefetch.*\$#image: kjwon15/qdon-prefetch:${git_version}#" docker-compose.yml > docker-stack.yml
+sed -e "s#image: ${IMAGE}.*\$#image: ${IMAGE_TAG}#" docker-compose.yml > docker-stack.yml
 docker stack deploy qdon-pfetch -c docker-stack.yml

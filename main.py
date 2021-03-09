6d533015424d6032d707913edc42b725bdda5bde
Jeong Arm <kjwonmail@gmail.com>
2021-03-09 16:51:55 +0900

    Print redirected url

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ class MediaFetcher(mastodon.StreamListener):
     def fetch(url):
         res = requests.get(url)
         s_print(
-            f'{url} {res.reason}'
+            f'{res.url} {res.reason}'
             f' {res.elapsed.total_seconds():.3f}'
             f' {res.headers["CF-Cache-Status"]} {res.headers["CF-Ray"]}')
 

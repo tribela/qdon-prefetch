583e32f0d372e09d6ed50c1bab23634afdf3c36c
kjwon15 <kjwonmail@gmail.com>
2021-05-08 15:03:05 +0900

    Support additional force IP

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ import requests
 import mastodon
 
 from get_docker_secret import get_docker_secret
+from adapters import HostHeaderSSLAdapter
 
+
+MASTODON_URL = get_docker_secret('MASTODON_URL')
+ACCESS_TOKEN = get_docker_secret('MASTODON_ACCESS_TOKEN')
+MEDIA_HOST = get_docker_secret('MEDIA_HOST')
+MEDIA_IPS = get_docker_secret('MEDIA_IPS').split(',')
 
 s_print_lock = Lock()
 
@@ -24,6 +30,22 @@ class MediaFetcher(mastodon.StreamListener):
         me = api.account_verify_credentials()
         s_print(f'I am {me.acct}')
 
+        self.sessions = [
+            requests.Session(),
+        ]
+
+        if MEDIA_HOST and MEDIA_IPS:
+            for ip in MEDIA_IPS:
+                s_print(f'Adding {ip}')
+                sess = requests.Session()
+                sess.mount(
+                    f'https://{MEDIA_HOST}/',
+                    HostHeaderSSLAdapter({
+                        MEDIA_HOST: ip,
+                    })
+                )
+                self.sessions.append(sess)
+
     def on_update(self, status):
         s_print(f'{status.id} {len(status.media_attachments)}')
 
@@ -38,25 +60,22 @@ class MediaFetcher(mastodon.StreamListener):
             self.fetch(media.url)
             self.fetch(media.preview_url)
 
-    @staticmethod
-    def fetch(url):
-        res = requests.get(url)
-        cache_status = res.headers.get('CF-Cache-Status', '')
-        ray = res.headers.get('CF-Ray', '')
+    def fetch(self, url):
+        for sess in self.sessions:
+            res = sess.get(url)
+            cache_status = res.headers.get('CF-Cache-Status', '')
+            ray = res.headers.get('CF-Ray', '')
 
-        s_print(
-            f'{res.url} {res.reason}'
-            f' {res.elapsed.total_seconds():.3f}'
-            f' {cache_status} {ray}')
+            s_print(
+                f'{res.url} {res.reason}'
+                f' {res.elapsed.total_seconds():.3f}'
+                f' {cache_status} {ray}')
 
 
 def stream_thread(target_function, listener):
     s_print(target_function.__name__)
     target_function(listener, reconnect_async=True)
 
-
-MASTODON_URL = get_docker_secret('MASTODON_URL')
-ACCESS_TOKEN = get_docker_secret('MASTODON_ACCESS_TOKEN')
 
 api = mastodon.Mastodon(
     api_base_url=MASTODON_URL,
